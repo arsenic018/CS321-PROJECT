6c9c566c152b6431085adf0dbd60aebf70d03e7b
Satjeev Radhakrishnan <satjeev.rk@gmail.com>
2026-04-05 21:46:39 -0400

    made pages unique

--- HotWars/src/pages/CreateSet.jsx
+++ HotWars/src/pages/CreateSet.jsx
@@ -5,32 +5,33 @@ const CreateSet = () => {
         <div className="startPanel">
             <header className="startHeader">
                 <h1 className="gameTitle">HOT WARS!</h1>
+                <h2 className="PageTitle"> This is the Create Set Screen</h2>
             </header>
-            <PlayButton/>
+            {/* <PlayButton/>
             <CreateButton/>
-            <LoadButton/>
+            <LoadButton/> */}
         </div>
         
     )
 }
-function PlayButton(){
-    return (
-        <div className="left-middle-button">
-        <button className="button-title">Play</button>
-        </div>
-    )
-}
-function CreateButton(){
-    return (<div className="right-top-button">
-        <button className="button-title">Create A Set</button>
-        </div>
-    )
-}
-function LoadButton(){
-    return (<div className="right-bottom-button">
-        <button className="button-title">Load A Set</button>
-        </div>
-    )
-}
+// function PlayButton(){
+//     return (
+//         <div className="left-middle-button">
+//         <button className="button-title">Play</button>
+//         </div>
+//     )
+// }
+// function CreateButton(){
+//     return (<div className="right-top-button">
+//         <button className="button-title">Create A Set</button>
+//         </div>
+//     )
+// }
+// function LoadButton(){
+//     return (<div className="right-bottom-button">
+//         <button className="button-title">Load A Set</button>
+//         </div>
+//     )
+// }
 
 export default CreateSet
--- HotWars/src/pages/LoadSet.jsx
+++ HotWars/src/pages/LoadSet.jsx
@@ -2,35 +2,36 @@ import React from "react"
 import './LoadSet.css';
 const LoadSet = () => {
     return(
-        <div className="startPanel">
+       <div className="startPanel">
             <header className="startHeader">
                 <h1 className="gameTitle">HOT WARS!</h1>
+                <h2 className="PageTitle"> This is the Load Set Screen</h2>
             </header>
-            <PlayButton/>
+            {/* <PlayButton/>
             <CreateButton/>
-            <LoadButton/>
+            <LoadButton/> */}
         </div>
         
     )
 }
-function PlayButton(){
-    return (
-        <div className="left-middle-button">
-        <button className="button-title">Play</button>
-        </div>
-    )
-}
-function CreateButton(){
-    return (<div className="right-top-button">
-        <button className="button-title">Create A Set</button>
-        </div>
-    )
-}
-function LoadButton(){
-    return (<div className="right-bottom-button">
-        <button className="button-title">Load A Set</button>
-        </div>
-    )
-}
+// function PlayButton(){
+//     return (
+//         <div className="left-middle-button">
+//         <button className="button-title">Play</button>
+//         </div>
+//     )
+// }
+// function CreateButton(){
+//     return (<div className="right-top-button">
+//         <button className="button-title">Create A Set</button>
+//         </div>
+//     )
+// }
+// function LoadButton(){
+//     return (<div className="right-bottom-button">
+//         <button className="button-title">Load A Set</button>
+//         </div>
+//     )
+// }
 
 export default LoadSet
