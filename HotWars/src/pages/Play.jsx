fff6998ee7d274f15367657884b44cedc6d3dae9
jjung88 <josephjung88@gmail.com>
2026-04-06 11:15:12 -0400

    Added Textbox to Play Page

--- HotWars/src/pages/Play.jsx
+++ HotWars/src/pages/Play.jsx
@@ -1,37 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './Play.css';
 const Play = () => {
-    return(
-    <div className="startPanel">
+    const [answer, setAnswer] = useState(""); // This will hold user Answer
+    const [score, setScore] = useState(0); // This will hold user Score
+    // FUTURE: Add a way to get questions from the loaded set the user provides. 
+    // I'm not sure what the best way to do this is yet, but I know we need to do it.
+
+    // Textbox handler
+    const handleAnswer = (event) => {
+        setAnswer(event.target.value); // Accesses the current text
+    };
+    // When we figure out how to retrieve the set of answers, we should add a handler
+    // that checks if the user's answer is correct.
+
+    return (
+        <div className="startPanel">
+
             <header className="startHeader">
                 <h1 className="gameTitle">HOT WARS!</h1>
                 <h2 className="PageTitle"> This is the Play Screen</h2>
             </header>
-            {/* <PlayButton/>
-            <CreateButton/>
-            <LoadButton/> */}
+
+            <Box display="flex" justifyContent="center" alignItems="center" >
+                <TextField id="filled-basic" label="Enter Answer" variant="filled"
+                    sx={{ backgroundColor: "white" }} />
+            </Box>
+
         </div>
-        
     )
 }
-// function PlayButton(){
-//     return (
-//         <div className="left-middle-button">
-//         <button className="button-title">Play</button>
-//         </div>
-//     )
-// }
-// function CreateButton(){
-//     return (<div className="right-top-button">
-//         <button className="button-title">Create A Set</button>
-//         </div>
-//     )
-// }
-// function LoadButton(){
-//     return (<div className="right-bottom-button">
-//         <button className="button-title">Load A Set</button>
-//         </div>
-//     )
-// }
 
 export default Play
