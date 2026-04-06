607217834de7573edc9d3944db268df266586145
Gurdeep Singh <gurdeepghotra0987@gmail.com>
2026-04-05 21:10:57 -0400

    Worked on page 2

--- HotWars/src/pages/Page2.jsx
+++ HotWars/src/pages/Page2.jsx
@@ -1,7 +1,36 @@
 import React from "react"
-
+import './Page2.css';
 const page2 = () => {
-    return <h1> Put the second page here Gurdeep</h1>
+    return(
+        <div className="startPanel">
+            <header className="startHeader">
+                <h1 className="gameTitle">HOT WARS!</h1>
+            </header>
+            <PlayButton/>
+            <CreateButton/>
+            <LoadButton/>
+        </div>
+        
+    )
+}
+function PlayButton(){
+    return (
+        <div className="left-middle-button">
+        <button className="button-title">Play</button>
+        </div>
+    )
+}
+function CreateButton(){
+    return (<div className="right-top-button">
+        <button className="button-title">Create A Set</button>
+        </div>
+    )
+}
+function LoadButton(){
+    return (<div className="right-bottom-button">
+        <button className="button-title">Load A Set</button>
+        </div>
+    )
 }
 
 export default page2
