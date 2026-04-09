6ff5e907a3668756d9c14b4ed93b0d821223ddac
EthanCooper100 <ecoope3@gmu.edu>
2026-04-08 23:21:50 -0400

    Removed bug

--- HotWars/src/pages/LoadSet.jsx
+++ HotWars/src/pages/LoadSet.jsx
@@ -16,9 +16,7 @@ const LoadSet = () => {
     return(
        <div className="startPanel">
             <header className="startHeader">
-                <h1 className="gameTitle">HOT WARS!</h1>
-                <h2 className="PageTitle"> This is the Load Set Screen</h2>
-                
+                <h1 className="gameTitle">HOT WARS!</h1>                
             </header>
             <LoadButton onClick={handleLoadSet} onPlay={() => navigate("/Play")} />
         </div>
